4b65477119157c5a11a78dc0787389dad6ee2beb
KJ1002 <nguyenvanphidmx123455@gmail.com>
2025-01-11 00:49:25 +0700

    optimize

--- src/apis/product.api.ts
+++ src/apis/product.api.ts
@@ -5,9 +5,7 @@ import http from 'src/utils/http'
 const URL = 'products'
 const productApi = {
   getProducts(params: ProductListConfig) {
-    return http.get<SuccessResponse<ProductList>>(URL, {
-      params
-    })
+    return http.get<SuccessResponse<ProductList>>(URL, { params })
   },
   getProductDetail(id: string) {
     return http.get<SuccessResponse<Product>>(`${URL}/${id}`)
